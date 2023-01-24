cd49c9c3eed6dc14bfae2518aba84d0504d8a9bc
Valentina Taborda <valentina18tabordarico@gmail.com>
2023-01-24 10:55:26 -0500

    Se pausa la canción

--- app/src/main/java/com/example/turistiando/Splash.java
+++ app/src/main/java/com/example/turistiando/Splash.java
@@ -39,6 +39,7 @@ public class Splash extends AppCompatActivity {
                 //Lanzar una nueva actividad
                 Intent intent = new Intent(Splash.this,MainActivity.class); //Al lugar donde quiero ir
                 startActivity(intent);
+                cancion.pause();
             }
         };
 
